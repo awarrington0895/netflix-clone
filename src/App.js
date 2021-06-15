7c85afdfa617a9273f453281501484d8203779d3
awarrington0895 <awarrington0895@gmail.com>
2021-06-15 02:07:58 +0000

    First data out of firebase

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
+import Row from "./components/Row";
 
 const App = () => (
   <div className="app">
     <Nav />
 
     <Header />
+    <Row title="Netflix Originals" type="netflix-originals" />
+    <Row title="Netflix Originals" movieType="netflix-originals" />
+    <Row title="Trending Now" movieType="trending-now" />
+    <Row title="Top Rated" movieType="top-rated" />
+    <Row title="Action Movies" movieType="action-movies" />
+    <Row title="Horror Movies" movieType="horror-movies" />
+    <Row title="Romance Movies" movieType="romance-movies" />
+    <Row title="Documentaries Movies" movieType="documentaries-movies" />
   </div>
-)
+);
 export default App;
