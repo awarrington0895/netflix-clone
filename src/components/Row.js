0e8c472edb3b0a3d57d7477dd3db6f48f371b14a
awarrington0895 <awarrington0895@gmail.com>
2021-06-15 02:14:41 +0000

    Refactor Row component

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,18 +2,28 @@ import React, { useEffect, useState } from "react";
 
 import { firebaseDatabase } from "../firebase/firebase";
 
+const leafRoot = "movies";
+
+const movieRef = (movieType) =>
+  firebaseDatabase.ref(`${leafRoot}/${movieType}`);
+
+const moviePoster = movie => (
+  <img
+    className="row__poster row__posterLarge"
+    src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
+    alt={movie.original_name}
+  />
+);
+
 const Row = ({ title, movieType }) => {
   const [movies, setMovies] = useState([]);
-  const leafRoot = "movies";
 
   useEffect(() => {
     fetchMovies(movieType);
   }, []);
 
   const fetchMovies = (movieType) => {
-    const movieRef = firebaseDatabase.ref(`${leafRoot}/${movieType}`);
-
-    movieRef.on("value", (snapshot) => {
+    movieRef(movieType).on("value", (snapshot) => {
       const movies = snapshot.val();
 
       if (movies && movies.length !== 0) {
@@ -26,13 +36,7 @@ const Row = ({ title, movieType }) => {
     <div className="row">
       <h2>{title}</h2>
       <div className="row__posters">
-        {movies.map((movie) => (
-          <img
-            className="row__poster row__posterLarge"
-            src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`}
-            alt={movie.original_name}
-          />
-        ))}
+        {movies.map(moviePoster)}
       </div>
     </div>
   );
