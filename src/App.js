5e6318b863c21e9b2870f84d849c9912f533f63b
awarrington0895 <awarrington0895@gmail.com>
2021-06-15 01:37:08 +0000

    Basic setup

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <div className="nav false">
+        <img
+          className="nav__logo"
+          src="https://www.freepnglogos.com/uploads/netflix-logo-0.png"
+          alt=""
+        />
+
+        <img
+          className="nav__avatar"
+          src="http://pngimg.com/uploads/netflix/netflix_PNG8.png"
+          alt=""
+        />
+      </div>
+    </div>
+  );
+}
+
+export default App;
